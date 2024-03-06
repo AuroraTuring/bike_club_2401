3f381b38ea3cc77a8faa3e80234c7101d1978000
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-06 12:37:55 -0700

    Refactor methods

--- lib/biker.rb
+++ lib/biker.rb
@@ -17,16 +17,17 @@ class Biker
 
   # A Biker will not log a ride if the ride's terrain does not match their acceptable terrain. They also won't log a ride if the ride's total distance is greater than the Biker's max distance.
   def log_ride(ride, distance)
-    @rides[ride] = [] if @rides[ride].nil?
+    return if !@acceptable_terrain.include?(ride.terrain)
+    return if ride.total_distance > @max_distance
+    @rides[ride] ||= []
+    # @rides[ride] = [] if @rides[ride].nil?
     @rides[ride] << distance
   end
 
   # A Biker can report its personal record for a specific ride. This is the lowest time recorded for a ride. This method will return false if the Biker hasn't completed the ride.
   def personal_record(ride)
-    best_ride = @rides.map do |ride|
-      ride[1].sort[0]
-      require 'pry'; binding.pry
-    end
+    @rides[ride] ||= []
+    @rides[ride].sort.first
   end
 
 end
