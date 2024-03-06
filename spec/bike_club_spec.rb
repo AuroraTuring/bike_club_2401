16bba3c82acdbdcd9d067395dcfcad50bc41e464
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-06 13:29:17 -0700

    Remove unnecessary parameter

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -4,7 +4,7 @@ require './lib/bike_club'
 
 RSpec.describe BikeClub do
   before(:each) do
-    @bike_club1 = BikeClub.new("Kool Kids Klub", "Georgie")
+    @bike_club1 = BikeClub.new("Kool Kids Klub")
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     @biker1 = Biker.new("Kenny", 30)
