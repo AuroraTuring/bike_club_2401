2d8fd6d6c93a55bfa0dfe8359ad726050ca01447
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-06 09:59:54 -0700

    Add missing @ and TDD

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -52,9 +52,12 @@ RSpec.describe Biker do
       @biker2.learn_terrain!(:gravel)
       @biker2.learn_terrain!(:hills)
 
-      expect(biker2.rides).to eq({ @ride2 => [65.0] })
-      expect(biker2.personal_record(ride2)).to eq(65.0)
-      expect(biker2.personal_record(ride1)).to eq(false)
+      @biker2.log_ride(@ride1, 95.0) # biker2 can't bike this distance
+      @biker2.log_ride(@ride2, 65.0) # biker2 knows this terrain and can bike this distance
+
+      expect(@biker2.rides).to eq({ @ride2 => [65.0] })
+      expect(@biker2.personal_record(@ride2)).to eq(65.0)
+      expect(@biker2.personal_record(@ride1)).to eq(false)
     end
   end
 
