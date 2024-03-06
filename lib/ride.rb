d76869db8b53306a4c68a69cf45042395a6365f5
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-06 09:18:53 -0700

    Set up attr_reader, initialize, TDD for exists and has attributes

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,11 @@
 class Ride
+  attr_reader :name, :distance, :loop, :terrain
+
+  def initialize(data)
+    @name = data[:name]
+    @distance = data[:distance]
+    @loop = data[:loop]
+    @terrain = data[:terrain]
+  end
 
 end
