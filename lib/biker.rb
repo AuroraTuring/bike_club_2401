404745a73af6787d3ab6f540c5b6b9b9cb5c4ffc
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-06 09:43:28 -0700

    Complete #log_ride

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,16 +1,23 @@
 class Biker
   attr_reader :name,
               :max_distance,
-              :acceptable_terrain
+              :acceptable_terrain,
+              :rides
 
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
     @acceptable_terrain = []
+    @rides = {}
   end
 
   def learn_terrain!(type)
     @acceptable_terrain << type
+  end
+
+  def log_ride(ride, distance)
+    @rides[ride] = [] if @rides[ride].nil?
+    @rides[ride] << distance
   end
 
 end
